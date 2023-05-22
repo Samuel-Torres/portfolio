e93ed3d16ddae6bdfeab6533497bf6b9132bc256
Samuel Torres <rilla@MacBook-Pro.local>
2023-05-22 13:37:45 -0400

    project setup, dependencies added, navbar creation

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch,
+//   Redirect,
+// } from "react-router-dom";
+
+function App() {
+  return (
+      <div>
+        <h1>HEADER</h1>
+      </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/navBar/navBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './navbar.scss';
+
+export const Navbar = () => {
+    return (
+        <div className="container">
+            <h2>NAVBAR</h2>
+        </div>
+    )
+}
+
+export default Navbar;
