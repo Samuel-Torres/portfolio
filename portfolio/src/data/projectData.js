20b8305e0528010693168693bcba2dc90a5d1736
Samuel Torres <rilla@MacBook-Pro.local>
2023-05-24 16:13:54 -0400

    Nav styling

--- portfolio/src/data/projectData.js
+++ portfolio/src/data/projectData.js
@@ -1,7 +1,19 @@
 export const projects = [
     {
         id: 0,
-        textLabel: 'Project 1',
-        
-    }
+        projectName: 'AirBnb Optimal Price Calculator',
+        projectDescription: 'iequrhfiuqhelriquehruflhqerlih',
+        projectRepositoryUrl: 'https://github.com/AirBnB-Optimal-Price-1-LS',
+        projectLiveUrl: 'https://airbnb-optimizedpricing.now.sh/login',
+        projectImgUrl: ''
+    },
+    {
+        id: 1,
+        projectName: 'Some Name',
+        projectDescription: 'qfvwevwrvwrtvwrt',
+        projectRepositoryUrl: 'www.github.com',
+        projectLiveUrl: 'www.facebook.com',
+        projectImgUrl: ''
+    },
+
 ]
---
+++ portfolio/src/routes/projects/ProjectPage.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import './projectPage.scss'
+import PageNav from "../../components/PageNav/PageNav";
+
+export const ProjectPage = (props) => {
+    const [ currentProject, setCurrentProject ] = useState();
+    const data = props.data;
+    const params = useParams();
+    
+    useEffect(() => {
+        setCurrentProject(data[params.id])
+    }, [params, data]);
+
+    return (
+        <div className="projectPageContainer">
+            <div className="projectContainer">
+                <h1>{currentProject.projectName}</h1>
+            </div>
+            <div className="pageNavContainer">
+                <h2>Projects</h2>
+                <PageNav data={data} />
+            </div>
+        </div>
+    )
+}
+
+export default ProjectPage;
